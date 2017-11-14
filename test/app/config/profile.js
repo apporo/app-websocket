e8a9fb22f2308dcf689ac8f359b014642370d433
pnhung177 <pnhung177@acegik.net>
2017-11-14 15:22:13 +0700

    Add websocket-example service

--- test/app/config/profile.js
+++ test/app/config/profile.js
@@ -15,6 +15,7 @@ module.exports = {
   },
   newFeatures: {
     application: {
+      logoliteEnabled: true,
       sandboxConfig: true
     }
   }
--- test/app/lib/services/websocket-example.js
+++ test/app/lib/services/websocket-example.js
@@ -5,6 +5,18 @@ var lodash = Devebot.require('lodash');
 var debugx =  Devebot.require('debug')('appWebsocket:example');
 
 var Service = function(params) {
+  var LX = params.loggingFactory.getLogger();
+  var LT = params.loggingFactory.getTracer();
+  params.websocketTrigger.addSocketHandler('example0', function(eventName, eventData, next) {
+    LX.isEnabledFor('debug') && LX.log('debug', LT.add({
+      message: 'Example receive message',
+      eventName: eventName,
+      eventData: eventData
+    }).toString());
+    next();
+  });
 }
+
+Service.referenceList = [ "websocketTrigger" ]
 
 module.exports = Service;

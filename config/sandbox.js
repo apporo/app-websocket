639b95a960885f630aa888912c0a81541fabcbf9
pnhung177 <pnhung177@acegik.net>
2018-04-19 15:46:18 +0700

    Define helper.getSocketById() method

--- config/sandbox.js
+++ config/sandbox.js
@@ -1,10 +1,6 @@
 module.exports = {
   plugins: {
     appWebsocket: {
-      host: '0.0.0.0',
-      port: 7878,
-      setPoweredBy: false,
-      printRequestInfo: false
     }
   }
 };
--- src/triggers/websocket-trigger.js
+++ src/triggers/websocket-trigger.js
@@ -1,20 +1,25 @@
 'use strict';
 
-var events = require('events');
-var util = require('util');
 var Devebot = require('devebot');
+var chores = Devebot.require('chores');
 var lodash = Devebot.require('lodash');
-var debugx = Devebot.require('pinbug')('appWebsocket:trigger');
 var socketIO = require('socket.io');
 
 var Service = function(params) {
-  debugx.enabled && debugx(' + constructor begin ...');
-
   params = params || {};
 
   var self = this;
   var LX = params.loggingFactory.getLogger();
   var LT = params.loggingFactory.getTracer();
+  var packageName = params.packageName || 'app-websocket';
+  var blockRef = chores.getBlockRef(__filename, packageName);
+
+  LX.has('silly') && LX.log('silly', LT.toMessage({
+    tags: [ blockRef, 'constructor-begin' ],
+    text: ' + constructor begin ...'
+  }));
+
+  var webserverTrigger = params["app-webserver/webserverTrigger"];
   var pluginCfg = lodash.get(params, ['sandboxConfig'], {});
 
   var socketHandler = {};
@@ -22,41 +27,56 @@ var Service = function(params) {
   self.addInterceptor = function(key, handler, options) {
     options = options || {};
     if (lodash.isString(key) && lodash.isFunction(handler)) {
-      debugx.enabled && debugx('addInterceptor("%s", <handler>)', key);
+      LX.has('trace') && LX.log('trace', LT.add({
+        key: key
+      }).toMessage({
+        tags: [ blockRef, 'add-interceptor' ],
+        text: ' - addInterceptor("${key}", /handler/)'
+      }));
       socketHandler[key] = socketHandler[key] || {};
       if (options.originalCallback === true) {
         socketHandler[key].interceptor = handler;
       } else {
         var eventMatcher = null;
         if (options.eventPattern instanceof RegExp) {
-          debugx.enabled && debugx('use default options.eventPattern');
+          LX.has('trace') && LX.log('trace', LT.toMessage({
+            tags: [ blockRef, 'event-pattern' ],
+            text: ' - use default options.eventPattern'
+          }));
           eventMatcher = options.eventPattern;
         } else if (typeof(options.eventPattern) === 'string') {
-          debugx.enabled && debugx('create pattern from "%s"', options.eventPattern);
+          LX.has('trace') && LX.log('trace', LT.add({
+            eventPattern: options.eventPattern
+          }).toMessage({
+            tags: [ blockRef, 'event-pattern' ],
+            text: ' - create pattern from "${eventPattern}"'
+          }));
           eventMatcher = new RegExp(options.eventPattern);
         }
         socketHandler[key].interceptor = function(packet, next) {
           var that = this;
           if (socketHandler[key].enabled == false) {
             next();
-            LX.isEnabledFor('debug') && LX.log('debug', that.tracer.add({
-              message: 'Interceptor is skipped (enabled ~ false)'
-            }).toMessage());
+            LX.has('debug') && LX.log('debug', that.tracer.toMessage({
+              text: 'Interceptor is skipped (enabled ~ false)'
+            }));
             return;
           }
           var eventName = (packet instanceof Array) && (packet.length > 0) && packet[0];
           var eventData = (packet instanceof Array) && (packet.length > 1) && packet[1];
-          if (LX.isEnabledFor('trace')) {
+          if (LX.has('trace')) {
             LX.log('trace', that.tracer.add({
-              message: 'Socket receives a packet',
               eventName: eventName,
               eventData: eventData
-            }).toMessage({reset: true}));
-          } else if (LX.isEnabledFor('debug')) {
+            }).toMessage({
+              text: 'Socket receives a packet'
+            }));
+          } else if (LX.has('debug')) {
             LX.log('debug', that.tracer.add({
-              message: 'Socket receives a packet',
               eventName: eventName
-            }).toMessage({reset: true}));
+            }).toMessage({
+              text: 'Socket receives a packet'
+            }));
           }
           if (eventMatcher == null || eventName == null || eventName.match(eventMatcher)) {
             handler.call(that, eventName, eventData, next);
@@ -66,33 +86,52 @@ var Service = function(params) {
         }
       }
     } else {
-      debugx.enabled && debugx('addInterceptor() - invalid parameters');
+      LX.has('debug') && LX.log('debug', LT.toMessage({
+        text: 'addInterceptor() - invalid parameters'
+      }));
     }
   };
 
   var io = socketIO();
+
+  this.helper = {};
+
+  this.helper.getSocketById = function(socketId, opts) {
+    opts = opts || {};
+    if (opts.isNamespaceSupported || opts.namespace) {
+      var ns = io && io.of(opts.namespace || "/");
+      if (ns && ns.connected) {
+        var socket = ns.connected[socketId];
+        return socket;
+      }
+    }
+    return io && io.sockets && io.sockets.sockets && io.sockets.sockets[socketId];
+  }
 
   io.on('connection', function (socket) {
     var socketId = socket.id;
     var clientIp = socket.request.connection.remoteAddress;
     var tracer = LT.branch({ key: 'socketId', value: socketId });
 
-    LX.isEnabledFor('debug') && LX.log('debug', tracer.add({
-      message: 'Socket server received a new connection'
-    }).toMessage({reset: true}));
+    LX.has('debug') && LX.log('debug', tracer.toMessage({
+      text: 'Socket server received a new connection'
+    }));
 
     lodash.forOwn(socketHandler, function(handler, key) {
       var subTracer = tracer.branch({ key: 'handlerId', value: key });
-      LX.isEnabledFor('debug') && LX.log('debug', subTracer.add({
-        message: 'Register interceptor with socket'
-      }).toMessage());
+      LX.has('debug') && LX.log('debug', subTracer.toMessage({
+        text: 'Register interceptor[${handlerId}] with socket[${socketId}]'
+      }));
       if (handler && lodash.isFunction(handler.interceptor)) {
-        socket.use(handler.interceptor.bind({ socket: socket, tracer: subTracer }));
+        socket.use(handler.interceptor.bind({
+          socket: socket,
+          tracer: subTracer
+        }));
       }
     });
   });
 
-  io.listen(params.webserverTrigger.server);
+  io.listen(webserverTrigger.server);
 
   self.start = function() {
     return Promise.resolve();
@@ -102,9 +141,12 @@ var Service = function(params) {
     return Promise.resolve();
   };
 
-  debugx.enabled && debugx(' - constructor end!');
+  LX.has('silly') && LX.log('silly', LT.toMessage({
+    tags: [ blockRef, 'constructor-end' ],
+    text: ' - constructor end!'
+  }));
 };
 
-Service.referenceList = [ "webserverTrigger" ];
+Service.referenceList = [ "app-webserver/webserverTrigger" ];
 
 module.exports = Service;
